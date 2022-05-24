647f3fa87a7f5d22c779ff018608ef5a53ca2f4f
rodrigo brinate <rbp20199@gmail.com>
2022-05-24 20:51:16 -0300

    versão estavel

--- src/pages/Historic.js
+++ src/pages/Historic.js
@@ -5,15 +5,24 @@ import axios from 'axios';
 
 function Login() {
   const api = axios.create({
-    baseURL: "https://acesse-cafe-back.herokuapp.com/",
-    //baseURL: "http://localhost:3001"
+    //baseURL: "https://acesse-cafe-back.herokuapp.com/",
+    baseURL: "http://localhost:3001"
   });
 const [coffee, setCoffee] = useState([])
+const [coffee2, setCoffee2] = useState([])
 const [msg, setMsg] = useState("")
   useEffect(() => {
     api
       .get("/historic")
       .then((response) => setCoffee(response.data)) //response.data.map((item)=> console.log(item.colaborador)))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+  useEffect(() => {
+    api
+      .get("/historic2")
+      .then((response) => setCoffee2(response.data)) //response.data.map((item)=> console.log(item.colaborador)))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
@@ -34,6 +43,13 @@ const [msg, setMsg] = useState("")
       <h1>Colaboradores no café hoje</h1>
       <ul>
       { coffee.map( (item) => <li>{ item.colaborador }<span>{(((item.minutos )/1000)/60).toFixed(0) /*((((new Date().getTime() - new Date(item.data).getTime())/1000)/60)/60).toFixed(0)+":"+Math.floor((((new Date().getTime() - new Date(item.data).getTime())/1000)/60).toFixed(0)%60)+":"+ Math.floor((((new Date().getTime() - new Date(item.data).getTime())/1000)%60)) */}</span></li>) }
+      </ul>
+    </div>
+
+    <div className="list-coffee">
+      <h1>Colaboradores no café hoje</h1>
+      <ul>
+      { coffee2.map( (item) => <li>{ item.colaborador }<span>{(((item.minutos )/1000)/60).toFixed(0) /*((((new Date().getTime() - new Date(item.data).getTime())/1000)/60)/60).toFixed(0)+":"+Math.floor((((new Date().getTime() - new Date(item.data).getTime())/1000)/60).toFixed(0)%60)+":"+ Math.floor((((new Date().getTime() - new Date(item.data).getTime())/1000)%60)) */}</span></li>) }
       </ul>
     </div>
  
